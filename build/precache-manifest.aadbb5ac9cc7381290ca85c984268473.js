fdda6f0cd25c45258c9204622fb0c151fb52ef45
Vishwanath Patil <vishwanath.patil@sjsu.edu>
2019-11-02 22:14:55 -0700

    deploy

---
+++ build/precache-manifest.aadbb5ac9cc7381290ca85c984268473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "347f83b22b921ec726e1",
+    "url": "/Portfolio/static/js/main.347f83b2.chunk.js"
+  },
+  {
+    "revision": "549149764a4abda863bc",
+    "url": "/Portfolio/static/js/1.54914976.chunk.js"
+  },
+  {
+    "revision": "347f83b22b921ec726e1",
+    "url": "/Portfolio/static/css/main.23b5688a.chunk.css"
+  },
+  {
+    "revision": "ca0de93343fa98eaef295ca4c4a48bc1",
+    "url": "/Portfolio/index.html"
+  }
+];
